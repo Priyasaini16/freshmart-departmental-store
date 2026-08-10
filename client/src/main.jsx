79fb243899da1cb7a18e3477c4eeb1c41adb82d2
Priya <sainipriya85571@gmail.com>
2026-08-10 10:28:36 +0530

    Fix WishlistContext import casing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import CartProvider from "./context/CartContext";
-import { WishlistProvider } from "./context/WishlistContext";
+import { WishlistProvider } from "./context/wishlistContext";
 import { ToastProvider } from "./context/toastContext";
 
 createRoot(document.getElementById("root")).render(
